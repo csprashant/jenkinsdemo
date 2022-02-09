41ac352602f11bbd079a4f598feb0d9ab8a4b0e8
csprashant <csprashantphuse@gmail.com>
2022-02-09 14:28:17 +0530

    first commit

---
+++ src/main/java/com/example/demo/model/student.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+public class student {
+private String name;
+private String  add;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getAdd() {
+	return add;
+}
+public void setAdd(String add) {
+	this.add = add;
+}
+}
